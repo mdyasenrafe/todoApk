714d07c7367646ea89d4e2232c0292841644507c
Md yasen rafe <mdyasenrafe@gmail.com>
2022-07-15 22:52:45 +0600

    todo api added

--- src/API/Index.js
+++ src/API/Index.js
@@ -5,6 +5,45 @@ const url = "https://todoapkapi.herokuapp.com/";
 const apiUrl = {
   signUp: "login/signup",
   signIn: "login/signin",
+  getTodo: "todo/get",
+  createTodo: "todo/create",
+  updateTodo: "todo/update",
+  deleteTodo: "todo/delete",
+};
+
+export const deleteTodoApi = async (body) => {
+  try {
+    const res = await axios.post(url + apiUrl.deleteTodo, body);
+    return res.data;
+  } catch (err) {
+    console.error({ err });
+  }
+};
+
+export const updateTodoApi = async (body) => {
+  try {
+    const res = await axios.post(url + apiUrl.updateTodo, body);
+    return res.data;
+  } catch (err) {
+    console.error({ err });
+  }
+};
+
+export const createTodoApi = async (body) => {
+  try {
+    const res = await axios.post(url + apiUrl.createTodo, body);
+    return res.data;
+  } catch (err) {
+    console.error({ err });
+  }
+};
+export const getTodoApi = async (body) => {
+  try {
+    const res = await axios.post(url + apiUrl.getTodo, body);
+    return res.data;
+  } catch (err) {
+    console.error({ err });
+  }
 };
 
 export const signUpApi = async (body) => {
